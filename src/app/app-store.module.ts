d4a20be2f54ec4f103112b39473dfbb5260b835c
halvlions99 <shalvorson75@gmail.com>
2019-06-17 15:13:43 -0500

    Comment out devtools

--- src/app/app-store.module.ts
+++ src/app/app-store.module.ts
@@ -1,10 +1,9 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
-import { Store, StoreModule, ActionReducer, MetaReducer } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
-import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+// import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { reducers } from './app.reducer';
 
@@ -30,7 +29,7 @@ const stores = [
     RouterModule,
     HttpClientModule,
     StoreModule.forRoot(reducers),
-    StoreDevtoolsModule.instrument({ maxAge: 100 }),
+   // StoreDevtoolsModule.instrument({ maxAge: 100 }),
     EffectsModule.forRoot([...effects])
   ],
   providers: [...services, ...stores],
