972bbf9f57608a9282cf147ecca716139a18aa01
halvlions99 <shalvorson75@gmail.com>
2019-06-26 12:10:02 -0500

    Dev tools back in

--- src/app/app-store.module.ts
+++ src/app/app-store.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
-// import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { reducers } from './app.reducer';
 
@@ -29,7 +29,7 @@ const stores = [
     RouterModule,
     HttpClientModule,
     StoreModule.forRoot(reducers),
-   // StoreDevtoolsModule.instrument({ maxAge: 100 }),
+    StoreDevtoolsModule.instrument({ maxAge: 100 }),
     EffectsModule.forRoot([...effects])
   ],
   providers: [...services, ...stores],
